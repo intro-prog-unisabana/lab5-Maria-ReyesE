a7c02783f2522f5469e12d14af21e7298f5bc1a9
María Reyes <marupaula.2004@gmail.com>
2026-03-17 18:10:39 -0500

    T

--- main.py
+++ main.py
@@ -10,38 +10,30 @@ while True:
         print("Invalid option!")
         continue
 
-    # 🔹 Operación con un solo número
     if op == "absolute":
         num = float(input("Enter the number:\n"))
         result = absolute(num)
-
-    # 🔹 Operaciones con dos números
     else:
         num1 = float(input("Enter the first number:\n"))
         num2 = float(input("Enter the second number:\n"))
 
         if op == "add":
             result = add(num1, num2)
-
         elif op == "subtract":
             result = sub(num1, num2)
-
         elif op == "multiply":
             result = multiply(num1, num2)
-
         elif op == "divide":
             result = divide(num1, num2)
-
         elif op == "exponent":
             result = exponent(num1, num2)
-
         elif op == "modulo":
             result = modulo(num1, num2)
-
         elif op == "floor_divide":
             result = floor_divide(num1, num2)
+
 
     if isinstance(result, str):
         print(result)
     else:
-        print("The result is:", result)
+        print("The result is:", float(result))
