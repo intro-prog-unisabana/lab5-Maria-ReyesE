829d0c5ce1617432e0847d5fe2182aa5d2e29000
María Reyes <marupaula.2004@gmail.com>
2026-03-17 18:37:58 -0500

    T

---
+++ utils_calc.py
@@ -0,0 +1,18 @@
+# FREEZE CODE BEGIN
+def greet(name):
+    return f"Hello, {name}!"
+
+def flip(input_string):
+    return input_string[::-1]
+
+def count_letters(input_string, letter):
+    count = 0
+    for char in input_string:
+        if char == letter:
+            count += 1
+    return count
+
+if __name__ == "__main__":
+    print("This file is being run directly.")
+# FREEZE CODE END
+
